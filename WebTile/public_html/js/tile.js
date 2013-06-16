2c2c73ebf80cffea28b3b60dff0f3eb6319b6f12
dusatzde <zdenek.dusatko@gmail.com>
2013-06-16 14:10:19 +0200

    - JSON data model - Mobile version redirection
    Signed-off-by: dusatzde <zdenek.dusatko@gmail.com>

---
+++ WebTile/public_html/js/tile.js
@@ -0,0 +1,37 @@
+function getParam(paramName) {
+    var QS = window.location.toString();
+    var indSta = QS.indexOf(paramName + "=");
+    if (indSta == -1 || paramName == "")
+        return null;
+    var indEnd = QS.indexOf('&', indSta);
+    if (indEnd == -1)
+        indEnd = QS.length;
+    var valore = unescape(QS.substring(indSta + paramName.length + 1, indEnd));
+    return valore;
+}
+
+function redirect() {
+    if (document.referrer.indexOf('hawks.mysteria.cz/') < 1) {
+        var url = getParam('url');
+        window.location.replace(url);
+        return true;
+    }
+    return false;
+}
+
+function render() {
+    var ico = getParam('ico');
+    document.getElementById('tile-img').src = 'img/tile/' + ico + '.png';
+    var theme = getParam('theme');
+    document.body.style.backgroundColor = theme;
+    if (theme === 'white') {
+        document.getElementById('tile-inst').src = 'img/inst_black.png';
+    } else {
+        document.getElementById('tile-inst').src = 'img/inst.png';
+    }
+}
+
+
+
+
+
