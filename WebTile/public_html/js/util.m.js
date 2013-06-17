7156f3085c55415467d85da6268e1904a9957245
dusatzde <zdenek.dusatko@gmail.com>
2013-06-17 20:12:20 +0200

    Custom tile creator.
    Signed-off-by: dusatzde <zdenek.dusatko@gmail.com>

---
+++ WebTile/public_html/js/util.m.js
@@ -0,0 +1,91 @@
+$(document).ready(function() {
+    $('#top').click(function() {
+        $('#partial-view').load('../partials/m/top.html');
+        $('#top').addClass('active');
+        $('#custom').removeClass('active');
+    });
+
+    $('#custom').click(function() {
+        $('#partial-view').load('../partials/m/custom.html', function() {
+            registerCustomHandlers();
+        });
+
+        $('#custom').addClass('active');
+        $('#top').removeClass('active');
+    });
+
+    !function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (!d.getElementById(id)) {
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "https://platform.twitter.com/widgets.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }
+    }(document, "script", "twitter-wjs");
+
+    (function() {
+        var po = document.createElement('script');
+        po.type = 'text/javascript';
+        po.async = true;
+        po.src = 'http://apis.google.com/js/plusone.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(po, s);
+    })();
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id))
+            return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/us_EN/all.js#xfbml=1";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+
+});
+
+function registerCustomHandlers() {
+
+    $("a.item").click(function() {
+        $(this).addClass("selected").siblings().removeClass("selected");
+    });
+
+
+    $('#submit-btn').click(function() {
+        var path = createPath();
+        window.location.replace(path);
+        return false;
+    });
+}
+
+function loadTop() {
+    $('#partial-view').load('../partials/m/top.html');
+    $('#top').addClass('active');
+}
+;
+
+function createPath() {
+    var userUrl = $('#user-url').val();
+    userUrl = checkUrl(userUrl);
+    var ico = 'web';
+    if($('.selected').length > 0){
+         ico = $('.selected')[0].getAttribute("id");
+    }
+
+    var path = "/tile.html?theme=Highlight&ico=" + ico + "&url=" + userUrl;
+    return path;
+};
+
+function checkUrl(userUrl){
+    if(userUrl.length === 0){
+        return "http://www.hawks.mysteria.cz";
+    }
+    var index = userUrl.indexOf("http");
+    if(index !== 0){
+         userUrl = "http://" + userUrl;
+    }
+    return userUrl;
+}
+
